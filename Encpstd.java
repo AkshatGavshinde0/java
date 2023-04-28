1a7ffdd476f68f022f5330795d327d85b6cf7fa2
AkshatGavshinde0 <128071412+AkshatGavshinde0@users.noreply.github.com>
2023-04-28 10:32:52 +0530

    Add files via upload
    encapsulation programs

---
+++ Encpstd.java
@@ -0,0 +1,28 @@
+class Student {
+    private int Student_Id;
+    private String name;
+   
+    public int getId() {
+        return Student_Id;
+    }
+    public void setId(int s_id) {
+        this.Student_Id = s_id;
+    }
+    public String getname() {
+        return name;
+    }
+    public void setname(String s_name) {
+        this.name = s_name;
+    }
+}
+ class Encpstd{
+    public static void main(String[] args) {
+        
+        Student s=new Student();
+        s.setId (27);
+        s.setname("Tom Lee");
+            
+        System.out.println("Student Data:" + "\nStudent ID:" + s.getId()
+                            + " Student Name:" + s.getname());
+    }
+}
